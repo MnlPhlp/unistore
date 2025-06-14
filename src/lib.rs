b8555f83c06a2fcdcfe966e52335cdf505049ed9
MnlPhlp <online@philipp-manuel.de>
2025-06-14 17:43:04 +0200

    abitility for multiple object stores

--- src/lib.rs
+++ src/lib.rs
@@ -3,13 +3,32 @@ mod native;
 // #[cfg(target_arch = "wasm32")]
 mod wasm;
 
-use std::{marker::PhantomData, sync::OnceLock};
+use std::marker::PhantomData;
 
 use serde::{Serialize, de::DeserializeOwned};
 
-pub struct UniStore<K, V> {
+pub trait Key: Serialize + DeserializeOwned {}
+impl<T: Serialize + DeserializeOwned> Key for T {}
+
+pub trait Value: Serialize + DeserializeOwned {}
+impl<T: Serialize + DeserializeOwned> Value for T {}
+
+pub struct UniStore {
     // #[cfg(target_arch = "wasm32")]
     db: wasm::Database,
+    name: String,
+}
+impl std::fmt::Debug for UniStore {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.debug_struct("UniStore")
+            .field("name", &self.name)
+            .finish_non_exhaustive()
+    }
+}
+
+#[derive(Debug)]
+pub struct UniTable<'a, K: Key, V: Value> {
+    store: &'a UniStore,
     name: String,
     phantom: PhantomData<(K, V)>,
 }
@@ -18,6 +37,8 @@ pub struct UniStore<K, V> {
 pub enum Error {
     #[cfg(target_arch = "wasm32")]
     Wasm(#[from] wasm::Error),
+    #[cfg(not(target_arch = "wasm32"))]
+    Native(#[from] native::Error),
 }
 impl std::fmt::Display for Error {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
@@ -25,11 +46,7 @@ impl std::fmt::Display for Error {
     }
 }
 
-impl<K, V> UniStore<K, V>
-where
-    K: Serialize,
-    V: Serialize + DeserializeOwned,
-{
+impl UniStore {
     pub async fn new(name: &str) -> Result<Self, Error> {
         #[cfg(target_arch = "wasm32")]
         let db = wasm::create_database(name).await?;
@@ -38,10 +55,23 @@ where
         Ok(UniStore {
             db,
             name: name.to_string(),
-            phantom: PhantomData,
         })
     }
 
+    pub async fn create_table<K: Key, V: Value>(
+        &self,
+        name: &str,
+        replace_if_incompatible: bool,
+    ) -> Result<UniTable<K, V>, Error> {
+        #[cfg(target_arch = "wasm32")]
+        let table = wasm::create_table(self, name, replace_if_incompatible).await?;
+        #[cfg(not(target_arch = "wasm32"))]
+        let table = native::create_table(self, name, replace_if_incompatible).await?;
+        Ok(table)
+    }
+}
+
+impl<K: Key, V: Value> UniTable<'_, K, V> {
     pub async fn insert(&self, key: K, value: V) -> Result<(), Error> {
         #[cfg(target_arch = "wasm32")]
         wasm::insert(self, key, value).await?;
@@ -59,4 +89,50 @@ where
         let value = wasm::get(self, key).await?;
         Ok(value)
     }
+}
+
+#[macro_export]
+macro_rules! static_table {
+    ($fn_name:ident, $name:literal, $key:ty, $val:ty, $get_store: ident) => {
+        async fn $fn_name() -> &'static UniTable<'static, $key, $val> {
+            static TABLE: OnceLock<UniTable<'static, $key, $val>> = OnceLock::new();
+            static INITIALIZING: Mutex<()> = Mutex::new(());
+
+            if let Some(table) = TABLE.get() {
+                return table;
+            }
+            let _lock = INITIALIZING.lock().await;
+            if let Some(table) = TABLE.get() {
+                return table;
+            }
+            let store = get_store().await;
+            let table = store
+                .create_table::<$key, $val>($name, true)
+                .await
+                .expect("Failed to create table");
+            TABLE.set(table).expect("Failed to set table");
+            TABLE.get().unwrap()
+        }
+    };
+}
+
+#[macro_export]
+macro_rules! static_store {
+    ($fn_name:ident, $name:literal) => {
+        async fn $fn_name() -> &'static UniStore {
+            static STORE: OnceLock<UniStore> = OnceLock::new();
+            static INITIALIZING: Mutex<()> = Mutex::new(());
+
+            if let Some(store) = STORE.get() {
+                return store;
+            }
+            let _lock = INITIALIZING.lock().await;
+            if let Some(store) = STORE.get() {
+                return store;
+            }
+            let store = UniStore::new($name).await.expect("Failed to create store");
+            STORE.set(store).expect("Failed to set store");
+            STORE.get().unwrap()
+        }
+    };
 }
--- src/wasm.rs
+++ src/wasm.rs
@@ -1,11 +1,12 @@
 use std::rc::Rc;
 
-use idb::{DatabaseEvent, Factory, ObjectStoreParams};
+use idb::{DatabaseEvent, Factory, KeyRange, ObjectStoreParams};
 use serde::{Serialize, de::DeserializeOwned};
 use serde_wasm_bindgen::Serializer;
 use std::sync::Mutex;
+use wasm_bindgen::{JsCast, JsValue};
 
-use crate::UniStore;
+use crate::{Key, UniStore, UniTable, Value};
 
 thread_local! {
     static DBS: Mutex<Vec<Rc<idb::Database>>> = Mutex::new(Vec::new());
@@ -18,6 +19,13 @@ impl Database {
             let dbs = dbs.lock().unwrap();
             dbs[self.0 as usize].clone() // Access the database by its index
         })
+    }
+
+    fn update(&self, db: idb::Database) {
+        DBS.with(|dbs| {
+            let mut dbs = dbs.lock().unwrap();
+            dbs[self.0 as usize] = Rc::new(db); // Update the database at the index
+        });
     }
 
     fn new(db: idb::Database) -> Self {
@@ -34,6 +42,8 @@ impl Database {
 pub enum Error {
     Idb(#[from] idb::Error),
     Serde(#[from] serde_wasm_bindgen::Error),
+    KeyTypeMismatch(serde_wasm_bindgen::Error),
+    ValueTypeMismatch(serde_wasm_bindgen::Error),
 }
 impl std::fmt::Display for Error {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
@@ -46,36 +56,89 @@ pub async fn create_database(name: &str) -> Result<Database, Error> {
     let factory = Factory::new()?;
 
     // Create an open request for the database
-    let mut open_request = factory.open(name, None).unwrap();
-
-    // Add an upgrade handler for database
-    let name = name.to_string();
-    open_request.on_upgrade_needed(move |event| {
-        // Get database instance from event
-        let database = event.database().unwrap();
-
-        // Prepare object store params
-        let store_params = ObjectStoreParams::new();
-
-        // Create object store
-        let _ = database.create_object_store(&name, store_params).unwrap();
-    });
+    let open_request = factory.open(name, None).unwrap();
 
     // `await` open request
     let db = open_request.await?;
     Ok(Database::new(db))
 }
 
-pub async fn insert<K: Serialize, V: Serialize>(
-    store: &UniStore<K, V>,
+pub async fn create_table<'a, K: Key, V: Value>(
+    store: &'a UniStore,
+    name: &str,
+    replace_if_incomatible: bool,
+) -> Result<UniTable<'a, K, V>, Error> {
+    let db = store.db.get_db();
+    let mut replace = false;
+
+    'exists_check: {
+        if db.store_names().iter().any(|s| s == name) {
+            // If the store already exists, check if the types match
+            let tx = db.transaction(&[name], idb::TransactionMode::ReadOnly)?;
+            let obj_store = tx.object_store(name)?;
+            if let Some(cursor) = obj_store.open_cursor(None, None)?.await? {
+                let key = cursor.key()?;
+                let value = cursor.value()?;
+                if let Err(e) = serde_wasm_bindgen::from_value::<K>(key) {
+                    if replace_if_incomatible {
+                        replace = true;
+                        break 'exists_check; // If we are replacing, break to create a new store
+                    }
+                    return Err(Error::KeyTypeMismatch(e));
+                }
+                if let Err(e) = serde_wasm_bindgen::from_value::<V>(value) {
+                    if replace_if_incomatible {
+                        replace = true;
+                        break 'exists_check; // If we are replacing, break to create a new store
+                    }
+                    return Err(Error::ValueTypeMismatch(e));
+                }
+            }
+            return Ok(UniTable {
+                store,
+                name: name.to_string(),
+                phantom: std::marker::PhantomData,
+            });
+        }
+    }
+
+    // else create a new object store
+    let store_params = ObjectStoreParams::new();
+    let version = db.version().expect("Failed to get database version");
+    db.close();
+
+    let mut open_request = Factory::new()?
+        .open(&store.name, Some(version + 1))
+        .unwrap();
+    let name_string = name.to_string();
+    open_request.on_upgrade_needed(move |event| {
+        let edb = event.database().unwrap();
+        if replace {
+            edb.delete_object_store(&name_string).unwrap();
+        }
+        let _ = edb.create_object_store(&name_string, store_params).unwrap();
+    });
+    let new_db = open_request.await?;
+    store.db.update(new_db);
+
+    Ok(UniTable {
+        store,
+        name: name.to_string(),
+        phantom: std::marker::PhantomData,
+    })
+}
+
+pub async fn insert<K: Key, V: Value>(
+    table: &UniTable<'_, K, V>,
     key: K,
     value: V,
 ) -> Result<(), Error> {
-    let tx = store
+    let tx = table
+        .store
         .db
         .get_db()
-        .transaction(&[store.name.as_str()], idb::TransactionMode::ReadWrite)?;
-    let store = tx.object_store(&store.name)?;
+        .transaction(&[table.name.as_str()], idb::TransactionMode::ReadWrite)?;
+    let store = tx.object_store(&table.name)?;
     let value = &value.serialize(&Serializer::json_compatible()).unwrap();
     let key = &key.serialize(&Serializer::json_compatible()).unwrap();
     store.add(value, Some(key))?.await?;
@@ -83,26 +146,25 @@ pub async fn insert<K: Serialize, V: Serialize>(
     Ok(())
 }
 
-pub async fn contains<K: Serialize, V>(store: &UniStore<K, V>, key: K) -> Result<bool, Error> {
-    let tx = store
+pub async fn contains<K: Key, V: Value>(table: &UniTable<'_, K, V>, key: K) -> Result<bool, Error> {
+    let tx = table
+        .store
         .db
         .get_db()
-        .transaction(&[store.name.as_str()], idb::TransactionMode::ReadOnly)?;
-    let store = tx.object_store(&store.name)?;
+        .transaction(&[table.name.as_str()], idb::TransactionMode::ReadOnly)?;
+    let store = tx.object_store(&table.name)?;
     let key = key.serialize(&Serializer::json_compatible()).unwrap();
     let result = store.get(key)?.await?;
     Ok(result.is_some())
 }
 
-pub async fn get<K: Serialize, V: DeserializeOwned>(
-    store: &UniStore<K, V>,
-    key: K,
-) -> Result<Option<V>, Error> {
-    let tx = store
+pub async fn get<K: Key, V: Value>(table: &UniTable<'_, K, V>, key: K) -> Result<Option<V>, Error> {
+    let tx = table
+        .store
         .db
         .get_db()
-        .transaction(&[store.name.as_str()], idb::TransactionMode::ReadOnly)?;
-    let store = tx.object_store(&store.name)?;
+        .transaction(&[table.name.as_str()], idb::TransactionMode::ReadOnly)?;
+    let store = tx.object_store(&table.name)?;
     let key = key.serialize(&Serializer::json_compatible()).unwrap();
     let result = store.get(key)?.await?;
     if let Some(value) = result {
